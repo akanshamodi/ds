b10be7c53ece1a28d44a6cdcde7f0bdacbaba0c8
91808 <akanshamodi72@gmail.com>
2020-09-25 18:28:07 +0530

    pair added

--- src/Stack/StockSpan.java
+++ src/Stack/StockSpan.java
@@ -4,30 +4,37 @@ import java.util.Stack;
 
 public class StockSpan {
     public static int[] NLFL(int[] arr){
-        Stack<pair(Integer,Integer)> stack = new Stack<>();
+        Stack<Pair<Integer,Integer>> stack = new Stack<>();
         int brr[] = new int[arr.length];
         for(int i=0;i<arr.length;i++){
             if(stack.isEmpty()){
                 brr[i]=-1;
             }
-            else if(stack.peek()<=arr[i]){
+            else if(stack.peek().second<=arr[i]){
                 while(!stack.isEmpty()){
                     stack.pop();
                     if(stack.isEmpty()){
                         brr[i]=-1;
                         break;
                     }
-                    else if(stack.peek()>arr[i]){
-                        brr[i] = stack.peek();
+                    else if(stack.peek().second>arr[i]){
+                        brr[i] = stack.peek().first;
                         break;
                     }
                 }
             }
             else{
-                brr[i] = stack.peek();
+                brr[i] = stack.peek().first;
             }
-            stack.push(arr[i]);
+            stack.push(Pair.of(arr[i],i));
         }
         return brr;
+    }
+    public static void main(String args[]){
+        int arr[] = {100,80,60,70,60,75,85};
+        int index[] = NLFL(arr);
+        for(int i=0;i< index.length;i++){
+            System.out.println(i-index[i]);
+        }
     }
 }
